56b72f60b558cc029ec36a681627c189e491aff4
Pomianowski <kontakt@rapiddev.pl>
2020-04-20 20:33:38 +0200

    Init

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace Total_Print
+{
+    public partial class MainWindow : Window
+    {
+        private List<DocFile> docsList = new List<DocFile> { };
+        private string printerName;
+        public MainWindow()
+        {
+            InitializeComponent();
+            textBoxDirectory.Text = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments).ToString();
+            if (Directory.Exists(textBoxDirectory.Text))
+            {
+                ProcessDirectory(textBoxDirectory.Text);
+                filesList.ItemsSource = docsList;
+            }
+        }
+
+        private void TextBox_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            string path = PickFolderDialog(textBoxDirectory.Text, "Select folder for printing");
+            textBoxDirectory.Text = path;
+
+            if (Directory.Exists(path))
+            {
+                ProcessDirectory(path);
+                filesList.ItemsSource = docsList;
+            }
+        }
+        private void ProcessDirectory(string targetDirectory)
+        {
+            docsList = new List<DocFile> { };
+            // Process the list of files found in the directory.
+            string[] fileEntries = Directory.GetFiles(targetDirectory);
+            int i = 0;
+            foreach (string fileName in fileEntries)
+            {
+                if(System.IO.Path.GetExtension(fileName) == ".pdf")
+                    docsList.Add(new DocFile() { id = i++, name = System.IO.Path.GetFileName(fileName), type = System.IO.Path.GetExtension(fileName), path = fileName, isSelected = true });
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            progressBar.Visibility = Visibility.Visible;
+            PrintBuilder printer = new PrintBuilder()
+                .SetList(docsList.ToArray())
+                .SetPrinter(comboboxPrinter.SelectedItem.ToString());
+
+
+            if (printer.Ready())
+                printer.PrintAsync();
+
+            //printer.OnDone()
+            //progressBar.Visibility = Visibility.Hidden;
+        }
+        private string PickFolderDialog(string _def, string _name = "Select directory")
+        {
+            string path = "";
+            CommonOpenFileDialog _dialog = new CommonOpenFileDialog()
+            {
+                Title = _name,
+                IsFolderPicker = true,
+                InitialDirectory = _def,
+                DefaultDirectory = _def,
+                AddToMostRecentlyUsedList = false,
+                EnsureFileExists = true,
+                EnsurePathExists = true,
+                EnsureReadOnly = false,
+                EnsureValidNames = true,
+                Multiselect = false,
+                ShowPlacesList = true
+            };
+
+            if (_dialog.ShowDialog() == CommonFileDialogResult.Ok)
+                path = _dialog.FileName;
+            return path;
+        }
+        private void CheckBox_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            CheckBox check = sender as CheckBox;
+
+            int id = Convert.ToInt32(check.Tag.ToString());
+            //docsList[id].isSelected = (bool)check.IsChecked;
+        }
+    }
+}
---
+++ PrintBuilder.cs
@@ -0,0 +1,89 @@
+﻿using System.IO;
+using System.Drawing.Printing;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Total_Print
+{
+    public struct DocFile
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+        public string path { get; set; }
+        public bool isSelected { get; set; }
+    }
+
+    class PrintBuilder
+    {
+        DocFile[] docsList;
+
+        private PrinterSettings _printer;
+        private PageSettings _page;
+
+        public PrintBuilder SetPrinter(string printer)
+        {
+            this._printer = new PrinterSettings
+            {
+                PrinterName = printer,
+                Copies = (short)1,
+            };
+            this._page = new PageSettings(this._printer)
+            {
+                Margins = new Margins(0, 0, 0, 0),
+            };
+
+            return this;
+        }
+        public PrintBuilder SetList(DocFile[] files)
+        {
+            this.docsList = files;
+            return this;
+        }
+        public object OnDone(object t)
+        {
+            return t;
+        }
+        public bool Ready()
+        {
+            if (this.docsList != null && this._printer != null && this._page != null)
+                return true;
+            return false;
+        }
+        public async void PrintAsync()
+        {
+            if(this.Ready())
+            {
+                foreach (DocFile file in docsList)
+                {
+                    if (File.Exists(file.path) && file.isSelected)
+                        await Task.Run(() => PrintFile(file.path, file.name));
+                }
+            }
+        }
+        private void PrintFile(string path, string name = "Unknown PDF")
+        {
+            Thread.Sleep(100);
+
+            foreach (PaperSize paperSize in this._printer.PaperSizes)
+            {
+                if (paperSize.PaperName == name)
+                {
+                    this._page.PaperSize = paperSize;
+                    break;
+                }
+            }
+
+            using (var document = PdfiumViewer.PdfDocument.Load(path))
+            {
+                using (PrintDocument printDocument = document.CreatePrintDocument())
+                {
+                    printDocument.PrinterSettings = this._printer;
+                    printDocument.DefaultPageSettings = this._page;
+                    printDocument.PrintController = new StandardPrintController();
+                    printDocument.Print();
+                }
+            }
+        }
+    }
+}
