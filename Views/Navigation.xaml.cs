67f1a2c51b1211e1d9c3598bb2ee9b0c7a943307
Pomianowski <kontakt@rapiddev.pl>
2020-04-21 12:48:03 +0200

    Navigation animation

--- Views/Navigation.xaml.cs
+++ Views/Navigation.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -9,8 +10,11 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Total_Print.Views
 {
@@ -21,13 +25,17 @@ namespace Total_Print.Views
     {
         Views.Main _main;
         Views.Settings _settings;
+
+        private bool _allowDirectNavigation = false;
+        private NavigatingCancelEventArgs _navArgs = null;
+        private Duration _duration = new Duration(TimeSpan.FromSeconds(1));
+        private double _oldHeight = 0;
         public Navigation()
         {
             InitializeComponent();
 
             this.OpenDashboard();
         }
-
         public void OpenDashboard()
         {
             if (_main == null)
@@ -39,6 +47,20 @@ namespace Total_Print.Views
             if (_settings == null)
                 _settings = new Settings();
             rootFrame.Navigate(_settings);
+        }
+        private void rootOnNavigating(object sender, NavigatingCancelEventArgs e)
+        {
+            ThicknessAnimation tAnim = new ThicknessAnimation() {
+                Duration = TimeSpan.FromSeconds(0.3),
+                DecelerationRatio = 0.7,
+                To = new Thickness(0, 0, 0, 0)
+            };
+            if (e.NavigationMode == NavigationMode.New)
+                tAnim.From = new Thickness(0, 100, 0, 0);
+            else if (e.NavigationMode == NavigationMode.Back)
+                tAnim.From = new Thickness(0, 0, 0, 100);
+
+            (e.Content as Page).BeginAnimation(MarginProperty, tAnim);
         }
     }
 }
